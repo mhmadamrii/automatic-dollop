658dc3fc282ac78df3248ea457f9959eb62e4fec
mhmadamrii <interceptorghost4@gmail.com>
2024-09-15 19:32:56 +0700

    chore: initialized pre-configured turbo repo

---
+++ apps/netflix/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@repo/ui/components/ui/button";
+
+export default function Page() {
+  return (
+    <main className="bg-blue-500">
+      <Button>Click me</Button>
+    </main>
+  );
+}
